b4148a4ab3b1d66ae2a3546ccf9a2b0aa947e321
ETIYACORP\ferid.cafer <ferid.cafer@etiya.com>
2018-03-29 14:32:32 +0300

    list animation

--- app/src/main/java/com/ferid/app/cleaner/adapter/ExplorerAdapter.java
+++ app/src/main/java/com/ferid/app/cleaner/adapter/ExplorerAdapter.java
@@ -22,6 +22,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -42,6 +44,9 @@ public class ExplorerAdapter extends RecyclerView.Adapter<ExplorerAdapter.ViewHo
     private ArrayList<Explorer> items;
 
     private AdapterListener adapterClickListener;
+
+    //for animation
+    private int lastPosition = -1;
 
     public ExplorerAdapter(Context context, ArrayList<Explorer> items) {
         this.context = context;
@@ -78,6 +83,13 @@ public class ExplorerAdapter extends RecyclerView.Adapter<ExplorerAdapter.ViewHo
         } else {
             viewHolder.checkBox.setImageResource(R.drawable.ic_check_box_blank);
             viewHolder.checkBox.setColorFilter(ContextCompat.getColor(context, R.color.darkGrey));
+        }
+
+        //play animation for once
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.list_anim);
+            viewHolder.itemView.startAnimation(animation);
+            lastPosition = position;
         }
     }
 
